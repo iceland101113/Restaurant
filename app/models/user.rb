90b823e237990d9acb2a4c4706389258a571df8b
iceland101113 <r02521115@ntu.edu.tw>
2018-01-16 14:59:28 +0800

    create avatar uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  mount_uploader :avatar, AvatarUploader
   has_many :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
