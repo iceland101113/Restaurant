0fcf7f3e853a2a6d5043b18010fa4ebb9fc06db2
iceland101113 <r02521115@ntu.edu.tw>
2018-01-17 22:56:22 +0800

    set up profile page and show the restaurants user have commented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = current_user
+    @restaurant_a = Restaurant.all
+    @commented_restaurants = @user.restaurants
+    @commented_restaurants_nd = @commented_restaurants.ids.uniq
+
+    @commented_restaurants_number = @commented_restaurants.ids.uniq.length
+    @commented_restaurants.each do |restaurant|
+
+    end
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update_attributes(user_params)
+    redirect_to user_path(@user)
+    flash[:notice] = "Your user profile was successfully updated!"
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :intro, :avatar)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   mount_uploader :avatar, AvatarUploader
   has_many :comments
+  has_many :restaurants, through: :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
