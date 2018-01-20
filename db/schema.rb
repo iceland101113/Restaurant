b408f08d40f59df9da06513f205ebada2ead6557
iceland101113 <r02521115@ntu.edu.tw>
2018-01-20 18:10:13 +0800

    add favorite_count column to restaurants and new ranking route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180120070427) do
+ActiveRecord::Schema.define(version: 20180120100509) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20180120070427) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.integer "category_id"
+    t.integer "favorite_count"
   end
 
   create_table "users", force: :cascade do |t|
