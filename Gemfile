5010ce0491fde270b1b83fb46ab1161c276196c4
iceland101113 <r02521115@ntu.edu.tw>
2018-01-25 22:40:24 +0800

    setup: user has many followrs and add favorited restaurants, followings and followers to user profile

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem 'bootstrap-sass', '~> 3.3.7'
 
 gem 'jquery-rails'
 
+gem 'rollbar'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,16 @@ class UsersController < ApplicationController
     @commented_restaurants_nd = @commented_restaurants.ids.uniq
 
     @commented_restaurants_number = @commented_restaurants.ids.uniq.length
-    @commented_restaurants.each do |restaurant|
 
-    end
+    @favorite_restaurants = @user.favorited_restaurants
+    @favorite_restaurants_nd = @user.favorited_restaurants.ids.length
+
+    @followings = @user.followings.all
+    @followings_nd = @user.followings.ids.length
+
+    @followers = @user.followers.all
+    @followers_nd = @user.followers.ids.length
+
   end
 
   def edit
